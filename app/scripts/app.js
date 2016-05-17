67108fcde7af9f1d3a4650e82d274973f3d0d599
nishantmadu <nishant.madu@gmail.com>
2016-05-17 18:21:01 +0530

    Populated the data from the API to the View

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,11 +3,11 @@ var angularApp = angular.module('cricketApp', ['ngResource','ngRoute']);
 angularApp.config(function ($routeProvider) {
   $routeProvider
   .when ('/', {
-    templateUrl: 'templates/main.html',
+    templateUrl: 'app/templates/main.html',
     controller: 'HomeController'
   })
   .when ('/matchdetails', {
-    templateUrl: 'templates/forecast.html',
+    templateUrl: 'details.html',
     controller: 'DetailsController'
   })
 });
@@ -15,22 +15,8 @@ angularApp.config(function ($routeProvider) {
 
 angularApp.controller("HomeController",['$resource','$filter',function($resource,$filter){
   var vm=this;
-  vm.getCityTemperature = function(){
-    console.log(vm.cityName);
-    var cricketResource = $resource('http://http://cricapi.com/api/cricket',
-    vm.cricketResponse = weatherResource.get();
+    var cricketResource = $resource('http://cricapi.com/api/cricket');
+    vm.cricketResponse = cricketResource.get();
     console.log(vm.cricketResponse);
-   }
+
 }]);
-
-angularApp.controller("ForecastController",function($resource){
-  var fm=this;
-
-  fm.getCityForecast = function(){
-    console.log(fm.cityName);
-    var forecastResource = $resource('http://api.openweathermap.org/data/2.5/forecast',
-      {q:fm.cityName,appid:'27d43832d2a4adcb97fcbfa23db130aa',cnt:10});
-    fm.forecastResponse = forecastResource.get();
-    console.log(fm.forecastResponse);
-   }
-});
